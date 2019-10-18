f053be18a01126307cfbf38931e14ecda5b0a8bf
孙诚 <sunchengsuncheng@gmail.com>
2019-10-18 18:02:09 +0800

    移除 g2

--- gui/src/views/stats.vue
+++ gui/src/views/stats.vue
@@ -1,5 +1,7 @@
 <template>
-  <page-header :name="name" :id="id" type="stats">123123</page-header>
+  <page-header :name="name" :id="id" type="stats">
+    <div id="c1"></div>
+  </page-header>
 </template>
 
 <script>
@@ -12,15 +14,40 @@ export default {
   },
   props: ["id", "name"],
   data() {
-    return {};
+    return {
+      chartData: [
+        { genre: "Sports", sold: 275 },
+        { genre: "Strategy", sold: 115 },
+        { genre: "Action", sold: 120 },
+        { genre: "Shooter", sold: 350 },
+        { genre: "Other", sold: 150 }
+      ]
+    };
   },
-  methods: {},
+  methods: {
+    async getStats() {
+      await this.axios.get(`v1/container/add/stats/${this.id}`);
+    }
+  },
+  watch: {
+    "getStats.length"() {
+      // Step 2: 载入数据源
+      this.chart.source(this.chartData);
+      // Step 3：创建图形语法，绘制柱状图，由 genre 和 sold 两个属性决定图形位置，genre 映射至 x 轴，sold 映射至 y 轴
+      this.chart
+        .interval()
+        .position("genre*sold")
+        .color("genre");
+      this.chart.render();
+    }
+  },
   async created() {
-    connection.on("monitorLog", log => {
+    connection.on("monitorStats", log => {
       this.pushLog(log);
     });
-
-    await this.axios.get(`v1/container/add/stats/${this.id}`);
+  },
+  mounted() {
+    
   }
 };
 </script>
