6600f37aaf04606901a108347d87fd47bb686065
孙诚 <sunchengsuncheng@gmail.com>
2019-10-21 09:00:22 +0800

    card

--- gui/src/views/stats.vue
+++ gui/src/views/stats.vue
@@ -1,21 +1,57 @@
 <template>
   <page-header :name="name" :id="id" type="stats">
-    <el-row :gutter="20">
-      <el-col :span="12">
-        <ve-line :data="chartCpu" :extend="extend"></ve-line>
+    <el-row>
+      <el-col :span="24">
+        <el-card class="chart" shadow="hover">
+          <div slot="header" class="clearfix">
+            <span>CPU per ({{current.cpu}}%)</span>
+          </div>
+          <ve-line
+            :tooltip-visible="false"
+            :height="height"
+            :data="chartCpu"
+            :extend="extend"
+            :legend-visible="false"
+            :settings="chartSettings"
+            :colors="['#749f83']"
+          ></ve-line>
+        </el-card>
       </el-col>
-      <!-- <el-col :span="12">
-        <ve-line :data="chartCpu" :extend="extend"></ve-line>
-      </el-col>-->
+      <el-col :span="12"></el-col>
     </el-row>
-    <!-- <el-row :gutter="20">
+    <br />
+    <el-row :gutter="10">
       <el-col :span="12">
-        <ve-line :data="chartCpu" :extend="extend"></ve-line>
+        <el-card class="chart" shadow="hover">
+          <div slot="header" class="clearfix">
+            <span>Memory per ({{current.memory}}%)</span>
+          </div>
+          <ve-line
+            :tooltip-visible="false"
+            :data="chartMemory"
+            :extend="extend"
+            :legend-visible="false"
+            :height="height"
+            :colors="['#012345']"
+          ></ve-line>
+        </el-card>
       </el-col>
       <el-col :span="12">
-        <ve-line :data="chartCpu" :extend="extend"></ve-line>
+        <el-card class="chart" shadow="hover">
+          <div slot="header" class="clearfix">
+            <span>Memory Value ({{current.memoryValue}}MB) -- limit {{current.maxMemory}}GB</span>
+          </div>
+          <ve-line
+            :tooltip-visible="false"
+            :data="chartMemoryValue"
+            :extend="extend"
+            :legend-visible="false"
+            :height="height"
+            :colors="['#c23531']"
+          ></ve-line>
+        </el-card>
       </el-col>
-    </el-row>-->
+    </el-row>
   </page-header>
 </template>
 
@@ -34,10 +70,28 @@ export default {
       "xAxis.0.axisLabel.rotate": 50
     };
     return {
+      chartSettings: {
+        // area: true
+      },
       chartCpu: {
-        columns: ["Time", "CPU", "Memory"],
+        columns: ["Time", "CPU"],
         rows: []
-      }
+      },
+      chartMemory: {
+        columns: ["Time", "Memory"],
+        rows: []
+      },
+      chartMemoryValue: {
+        columns: ["Time", "MemoryValue"],
+        rows: []
+      },
+      current: {
+        cpu: 0,
+        memory: 0,
+        memoryValue: 0,
+        maxMemory: 0
+      },
+      height: "360px"
     };
   },
   methods: {
@@ -50,24 +104,59 @@ export default {
   },
   watch: {},
   async created() {
+    window.console.log("on monitorstats");
     connection.on("monitorstats", stats => {
-      var cpu =
+      this.current.cpu = (
         ((stats.cpuStats.cpuUsage.totalUsage -
           stats.preCPUStats.cpuUsage.totalUsage) /
           (stats.cpuStats.systemUsage - stats.preCPUStats.systemUsage)) *
-        100;
-      var memory = (stats.memoryStats.usage / stats.memoryStats.limit) * 100;
+        100
+      ).toFixed(2);
+      this.current.memory = (
+        (stats.memoryStats.usage / stats.memoryStats.limit) *
+        100
+      ).toFixed(2);
       this.chartCpu.rows.push({
         Time: this.toTime(stats.read),
-        CPU: cpu.toFixed(2),
-        Memory: memory.toFixed(2)
+        CPU: this.current.cpu
       });
-      window.console.log(this.chartCpu.rows.length);
+      this.chartMemory.rows.push({
+        Time: this.toTime(stats.read),
+        Memory: this.current.memory
+      });
+      this.current.memoryValue = (
+        stats.memoryStats.usage /
+        1024 /
+        1024
+      ).toFixed(2);
+      this.chartMemoryValue.rows.push({
+        Time: this.toTime(stats.read),
+        MemoryValue: this.current.memoryValue
+      });
+      this.current.maxMemory = (
+        stats.memoryStats.limit /
+        1024 /
+        1024 /
+        1024
+      ).toFixed(2);
+
+      if (this.chartCpu.rows.length > 100) {
+        this.chartCpu.rows.shift();
+        this.chartMemory.rows.shift();
+        this.chartMemoryValue.rows.shift();
+      }
     });
     await this.getStats();
   },
   destroyed() {
-    connection.methods["monitorstats"] = null; // 解除监控
+    window.console.log("delete monitorstats");
+    // delete connection.methods.monitorstats; // 解除监控
   }
 };
 </script>
+
+<style >
+.chart .el-card__body {
+  padding: 0px;
+}
+</style>
