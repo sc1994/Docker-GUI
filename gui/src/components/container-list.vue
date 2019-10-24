9a57c214d7a6c7f7dc274b72e84be9c3b45e9658
孙诚 <sunchengsuncheng@gmail.com>
2019-10-24 16:55:40 +0800

    调整

--- gui/src/components/container-list.vue
+++ gui/src/components/container-list.vue
@@ -4,22 +4,19 @@
       <el-col :span="2">Status</el-col>
       <el-col :span="4">Image</el-col>
       <el-col :span="5">Command</el-col>
-      <el-col :span="4">Created</el-col>
+      <el-col :span="3">Created</el-col>
       <el-col :span="7">Ports</el-col>
+      <el-col :span="2">
+        <i class="el-icon-view breathe-div"></i>
+      </el-col>
       <!-- TODO: 展示容器大小 -->
     </el-row>
     <hr style="border-style: dashed;" />
     <el-row v-for="container in containers" :key="container.id" class="line-text">
       <el-col :span="2">
         <el-tooltip :content="container.status" placement="right">
-          <el-tag v-if="container.state=='running'" size="small ">
-            <i class="el-icon-sunny"></i>
-            {{container.state}}
-          </el-tag>
-          <el-tag type="danger" v-else size="small ">
-            <i class="el-icon-heavy-rain"></i>
-            {{container.state}}
-          </el-tag>
+          <el-tag v-if="container.state=='running'" size="small ">{{container.state}}</el-tag>
+          <el-tag type="danger" v-else size="small ">{{container.state}}</el-tag>
         </el-tooltip>
       </el-col>
       <el-col :span="4" :title="container.image">
@@ -27,7 +24,7 @@
         {{container.image}}
       </el-col>
       <el-col :span="5" :title="container.command">{{container.command}}</el-col>
-      <el-col :span="4">{{container.createdStr}}</el-col>
+      <el-col :span="3">{{container.createdStr}}</el-col>
       <el-col :span="7" :title="showPorts(container.ports)">{{showPorts(container.ports)}}&nbsp;</el-col>
       <el-col :span="2">
         <el-dropdown trigger="click">
@@ -119,5 +116,36 @@ export default {
   text-overflow: ellipsis;
   white-space: nowrap;
   overflow: hidden;
+}
+
+.breathe-div {
+  /* width: 100px;
+  height: 100px; */
+  border: 1px solid #fafafa;
+  border-radius: 50%;
+  text-align: center;
+  cursor: pointer;
+  /* margin: 150px auto; */
+  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.3);
+  overflow: hidden;
+  -webkit-animation-timing-function: ease-in-out;
+  -webkit-animation-name: breathe;
+  -webkit-animation-duration: 1500ms;
+  -webkit-animation-iteration-count: infinite;
+  -webkit-animation-direction: alternate;
+}
+
+@-webkit-keyframes breathe {
+  0% {
+    opacity: 0.4;
+    box-shadow: 0 1px 2px rgba(0, 147, 223, 0.4),
+      0 1px 1px rgba(0, 147, 223, 0.1) inset;
+  }
+
+  100% {
+    opacity: 1;
+    border: 1px solid rgba(59, 235, 235, 0.7);
+    box-shadow: 0 1px 20px #0093df, 0 1px 20px #0093df inset;
+  }
 }
 </style>
