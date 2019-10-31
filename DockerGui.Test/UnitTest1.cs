e796b68ccfbd8807994f438f4e84e23433cf81a6
孙诚 <sunchengsuncheng@gmail.com>
2019-10-31 15:46:42 +0800

    输出

--- DockerGui.Test/UnitTest1.cs
+++ DockerGui.Test/UnitTest1.cs
@@ -1,14 +1,21 @@
 using System;
+using System.Diagnostics;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace DockerGui.Test
 {
     public class UnitTest1
     {
+        private readonly ITestOutputHelper _output;
+        public UnitTest1(ITestOutputHelper output)
+        {
+            _output = output;
+        }
         [Fact]
         public void Test1()
         {
-
+            _output.WriteLine("123");
         }
     }
 }
